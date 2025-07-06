547c9241ebd6efdafda6ee3c3b8affabbfa5723a
mariusz woda <mariusz.woda@gmail.com>
2025-07-07 00:44:46 +0200

    initial commit: empty spring project

---
+++ demo-spring/src/test/java/pl/where2play/demospring/DemoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.where2play.demospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
